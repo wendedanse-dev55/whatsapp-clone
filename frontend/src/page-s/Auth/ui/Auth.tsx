24ba99525580b102e31577869e6711b7300ad747
Kutman Soronbaev <kutmansoronbaev@Kutmans-MacBook-Pro.local>
2024-05-12 13:10:04 +0600

    fx

--- frontend/src/page-s/Auth/ui/Auth.tsx
+++ frontend/src/page-s/Auth/ui/Auth.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from "./Auth.module.scss";
 import {PhoneNumber} from "@/features/PhoneNumber";
-export const Auth = () => {
+export const Auth = () => { 
     return (
         <div>
-            <PhoneNumber />
+            <PhoneNumber />  
         </div>
     );
 };
--- frontend/src/widgets/ChatContentFooter/ui/ChatContentFooter.tsx
+++ frontend/src/widgets/ChatContentFooter/ui/ChatContentFooter.tsx
@@ -1,18 +1,18 @@
-"use client";
-import React, { useEffect } from "react";
-import styles from "./ChatContentFooter.module.scss";
-import { CreateAudio } from "@/features/CreateAudio";
-import { createAudioHelpers } from "@/features/CreateAudio/ui/CreateAudio.helpers";
-import { useCounter } from "@/shared/hooks/useCounter";
-import { CanvasAudio } from "@/features/CanvasAudio";
+'use client'
+import React, { useEffect } from 'react'
+import styles from './ChatContentFooter.module.scss'
+import { CreateAudio } from '@/features/CreateAudio'
+import { createAudioHelpers } from '@/features/CreateAudio/ui/CreateAudio.helpers'
+import { useCounter } from '@/shared/hooks/useCounter'
+import { CanvasAudio } from '@/features/CanvasAudio'
 export const ChatContentFooter = () => {
-  const { isListen, setIsListen, handleClickMicrophone } = createAudioHelpers();
-  const { time, reset, start } = useCounter();
-  return (
-    <div className={styles.Footer}>
-      <canvas id="voiceAnimation" width="180" height="28"></canvas>
-      <CreateAudio />
-      <CanvasAudio />
-    </div>
-  );
-};
+	const { isListen, setIsListen, handleClickMicrophone } = createAudioHelpers()
+	const { time, reset, start } = useCounter()
+	return (
+		<div className={styles.Footer}>
+			<canvas id='voiceAnimation' width='180' height='28'></canvas>
+			<CreateAudio />
+			<CanvasAudio />
+		</div>
+	)
+}
